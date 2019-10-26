749fac16797ccb4f0a53e58c76bf5d6733c86062
EduardSemashko <edwardsemashko0197@gmail.com>
2019-10-26 14:30:11 +0200

    14/29

---
+++ MageJamProject/Assets/Scripts/Start.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start : MonoBehaviour
+{
+    
+    public Animator transitionAnim;
+    public float wait;
+    public void PlayGame(){
+        StartCoroutine(changer()); 
+        
+    }
+    public void Again(){
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void quit(){
+        Application.Quit();
+    }
+    IEnumerator changer(){
+        //transitionAnim.SetTrigger("end");
+        yield return new  WaitForSeconds(wait);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+
+    }
+}
+
+
